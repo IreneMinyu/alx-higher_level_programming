a736ef4d37c7f9751fde119f4894efe87dfe02f9
WangariM <wangariminyu@gmail.com>
2022-06-08 06:11:00 +0400

    function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+
 /**
- * is_palindrome - function to call check_pal to see if list is palindrome
- * @head: ptr to the beginning of the list
- * Return: 0 if not palindrome else 1
- */
-int is_palindrome(listint_t **head)
+* list_len - finds no. of elements ina linked list.
+* @h: pointer to linked list.
+*
+* Return: number of elements in linked list.
+*/
+size_t list_len(listint_t *h)
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (check_pal(head, *head));
+	size_t  nodes = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
 
 /**
- * check_pal - function to check if the list is palindrome
- * @head: ptr to the beginning of the list
- * @last: ptr to the end of the list
- * Return: 0 if not palindrom else 1
- */
-int check_pal(listint_t **head, listint_t *last)
+* is_palindrome - checks if a singly linked list is a palindrome.
+* @head: double pointert to head of d-list.
+*
+* Return: 1 if palindrome, 0 otherwise.
+*/
+int is_palindrome(listint_t **head)
 {
-	if (last == NULL)
+	int *nArr, i = 0, j = 0, len = 0;
+	listint_t *temp;
+
+	if (*head == NULL)
 		return (1);
-	if (check_pal(head, last->next) && (*head)->n == last->n)
+	temp = *head;
+	len = list_len(temp);
+	nArr = (int *)malloc(sizeof(int) * len);
+	if (nArr == NULL)
+		return (2);
+	temp = *head;
+	while (temp != NULL)
 	{
-		*head = (*head)->next;
-		return (1);
+		nArr[j] = temp->n;
+		j++;
+		temp = temp->next;
 	}
-	return (0);
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		if (nArr[i] != nArr[j])
+			return (0);
+	}
+	return (1);
 }
