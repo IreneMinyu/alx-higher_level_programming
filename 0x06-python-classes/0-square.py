73b42fe811caaf6f9a1f6da65629e6f409e5e832
WangariM <wangariminyu@gmail.com>
2022-06-22 01:16:10 +0400

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
 """
-Class that defines a square
+Empty class that defines a square
 """
 
 
 class Square:
     """
-    Square implementation
+    Square definition
     """
     pass
-
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Define square class
+Implement the value
+Calculate area and check for attributes
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
