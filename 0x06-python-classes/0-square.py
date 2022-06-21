14e62d65e013841291ca6fbd02f91e818294ee2d
WangariM <wangariminyu@gmail.com>
2022-06-21 23:39:28 +0400

    Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Square implementation
+    """
+    pass
+
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
