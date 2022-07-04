71b68460f630da1bca1a710ac9df5e930c9b132f
WangariM <wangariminyu@gmail.com>
2022-07-05 00:40:41 +0400

    Python - Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-Myint class
-"""
-
-
 class MyInt(int):
-    """"""
+    """ Class that inherits from class int"""
 
     def __eq__(self, other):
-        if isinstance(self, type(other)):
-            return False
+        """ Method that returns != check """
+        return int.__ne__(self, other)
 
     def __ne__(self, other):
-        if isinstance(self, type(other)):
-            return True
+        """ Method that returns == check """
+        return int.__eq__(self, other)
