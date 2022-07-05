d18780f9843c441473e95916240185a232282859
WangariM <wangariminyu@gmail.com>
2022-07-06 00:54:36 +0400

    Python - Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+"""write file"""
 
 
 def write_file(filename="", text=""):
-    """Write string to file
-    Args:
-        filename (str): string of path to file
-        text (str): string to write to file
-    Returns:
-        number of characters written
-    """
-    chars_written = 0
-    with open(filename, 'w', encoding='utf-8') as f:
-        chars_written += f.write(text)
-    return chars_written
+    """"write file"""
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(text)
+    return len(text)
+
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
